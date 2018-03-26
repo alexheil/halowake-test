84e606176e4bca3e8b7947f4ff89606c4caca258
alexheil <aheil4250@gmail.com>
2018-03-26 10:31:18 -0400

    test

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -1,12 +1,18 @@
 require "shrine"
-require "shrine/storage/file_system"
- 
+require "shrine/storage/s3"
+
 Shrine.plugin :activerecord
 Shrine.plugin :logging, logger: Rails.logger
+Shrine.plugin :validation_helpers
+ 
+s3_options = {
+    access_key_id:      ENV['S3_KEY'],
+    secret_access_key:  ENV['S3_SECRET'],
+    region:             ENV['S3_REGION'],
+    bucket:             ENV['S3_BUCKET'],
+}
  
 Shrine.storages = {
-  cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"),
-  store: Shrine::Storage::FileSystem.new("public", prefix: "uploads/store"),
+    cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"),
+    store: Shrine::Storage::S3.new(prefix: "store", **s3_options),
 }
-
-Shrine.plugin :validation_helpers
